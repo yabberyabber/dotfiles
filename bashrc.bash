498baf4cd13e8390bccd684fc6d0b0c5a1dd19a5
Andrew Nelson <andrew2nelson@gmail.com>
2017-03-16 18:30:53 -0700

    Update bashrc.bash
    add prompt

--- bashrc.bash
+++ bashrc.bash
@@ -9,3 +9,9 @@ alias unix14='ssh anelso32@unix14.csc.calpoly.edu'
 alias vogon='ssh anelso32@vogon.csc.calpoly.edu'
 alias foaad="unix13 -t 'ssh nelson466@frank.ored.calpoly.edu'"
 alias dekhtyar="unix13 -t 'ssh anelso32@cslvm57.csc.calpoly.edu'"
+
+if [ "$color_prompt" = yes ]; then
+    PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
+else
+    PS1='${debian_chroot:+($debian_chroot)}\u@\h:\w\$ '
+fi
